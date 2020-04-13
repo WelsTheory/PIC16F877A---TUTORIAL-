fb79891a3546b9c026cc73f990dea4375ccb7de7
Wels <williams.limonchi.sandoval@gmail.com>
2020-04-13 13:08:33 -0500

    Timer 1 - Temporizador

---
+++ Timer1_Temporizador.X/build/default/production/Programa_principal.o.d
@@ -0,0 +1,3 @@
+build/default/production/Programa_principal.o:  \
+	D:/Electronica/PIC16F877A\ -\ TUTORIAL/Timer1_Temporizador.X/Programa_principal.asm  \
+
---
+++ Timer1_Temporizador.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Timer1_Temporizador.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Timer1_Temporizador.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=timer1temporizador.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/timer1temporizador.x.tar
