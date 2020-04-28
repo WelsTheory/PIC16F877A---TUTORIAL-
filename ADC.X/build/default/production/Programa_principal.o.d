dc23840ae5359fe1eb7430f6e5424ce8dcf69414
Wels <williams.limonchi.sandoval@gmail.com>
2020-04-28 12:54:11 -0500

    Tutorial ADC

---
+++ ADC.X/build/default/production/Programa_principal.o.d
@@ -0,0 +1,4 @@
+build/default/production/Programa_principal.o:  \
+	D:/Electronica/PIC16F877A\ -\ TUTORIAL/ADC.X/Programa_principal.asm  \
+	D:/Electronica/PIC16F877A\ -\ TUTORIAL/ADC.X/Retardo.inc  \
+
