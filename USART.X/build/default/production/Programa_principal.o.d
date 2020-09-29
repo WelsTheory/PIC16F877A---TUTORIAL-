0c3dc267e5b6e2dd00361aa4a3bd2a16a1dd92d6
Williams Limonchi <williams.limonchi.sandoval@gmail.com>
2020-09-29 12:08:28 -0500

    USART

---
+++ USART.X/build/default/production/Programa_principal.o.d
@@ -0,0 +1,4 @@
+build/default/production/Programa_principal.o:  \
+	D:/Electronica/PIC16F877A\ -\ TUTORIAL/USART.X/Programa_principal.asm  \
+	D:/Electronica/PIC16F877A\ -\ TUTORIAL/USART.X/Retardo.inc  \
+
